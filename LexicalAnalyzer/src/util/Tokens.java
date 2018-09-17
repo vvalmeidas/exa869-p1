5d24e9d8cced529fd115a4b5d5066d98c8e32f6f
vvalmeida <vvalmeida96@gmail.com>
2018-09-16 21:57:52 -0300

    Add tokens list

---
+++ LexicalAnalyzer/src/util/Tokens.java
@@ -0,0 +1,23 @@
+package util;
+
+public class Tokens {
+	
+	/**
+	 * Tokens
+	 */
+	public static final String KEY_WORD = "KEY WORD";
+	public static final String IDENTIFIER = "IDENTIFIER";
+	public static final String NUMBER = "NUMBER";
+	public static final String ARITHMETIC_OP = "ARITHMETIC_OP";
+	public static final String RELATIONAL_OP = "RELATIONAL_OP";
+	public static final String LOGIC_OP = "LOGIC_OP";
+	public static final String DELIMITER = "DELIMITER";
+	public static final String STRING = "STRING";
+	
+	/**
+	 * Construtor privado
+	 */
+	private Tokens() {}
+	
+
+}
