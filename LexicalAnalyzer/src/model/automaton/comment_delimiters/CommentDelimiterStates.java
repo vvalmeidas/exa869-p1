260f3813a2d6ea66d71c3b9bf278f7d6118720fd
Nadine <nadymarkes@gmail.com>
2018-09-20 20:05:57 -0300

    Comment Delimiter Automaton Without Tests

--- LexicalAnalyzer/src/model/automaton/comment_delimiters/CommentDelimiterStates.java
+++ LexicalAnalyzer/src/model/automaton/comment_delimiters/CommentDelimiterStates.java
@@ -7,7 +7,9 @@ import model.automaton.State;
 import util.LexemeChecker;
 
 /**
- *
+ * Enumeração com o estado inicial e os estados intermediários do autômato de delimitadores
+ * de comentário e as respectivas implementações da lógica de transição de estado adequada. 
+ * 
  * @author Nadine Cerqueira
  * @author Valmir Vinicius
  *
@@ -18,31 +20,52 @@ public enum CommentDelimiterStates implements State{
 
 		@Override
 		public State next(char character) {
-			if(LexemeChecker.isCommentDelimiter(character)) {
-				return CORRECT_COMMENT_DELIMITER_STATE;
-			} else if(LexemeChecker.isBlockCommentDelimiter(character)) {
-				return SECOND_COMMENT_DELIMITER_STATE;
+			if(character == '/') {
+				return ASTERISC_COMMENT_DELIMITER_STATE;
 			}
-			
 			return CommentDelimiterFinalStates.NOT_COMMENT_DELIMITER_FINALSTATE;
 			
 		}
 		
 	},
-	SECOND_COMMENT_DELIMITER_STATE {
+	ASTERISC_COMMENT_DELIMITER_STATE {
 		@Override
 		public State next(char character) {
-//			while(character != '*/' || character != 'eof' || character != 'EOF') {
-//				return SECOND_COMMENT_DELIMITER_STATE;
-//			}
-//			if(character == 'eof' || character == 'EOF') {
-//				return CommentDelimiterFinalStates.BADLYFORMEDCOMMENT_DELIMITER_FINALSTATE;
-//			}
-//				
-			return CORRECT_COMMENT_DELIMITER_STATE;
+			if(character == '*') {
+				return SECOND_ASTERISC_COMMENT_DELIMITER_STATE;
+			} else if(character == '/') {
+				return CORRECT_COMMENT_DELIMITER_STATE;
+			}
+				
+			return CommentDelimiterFinalStates.NOT_COMMENT_DELIMITER_FINALSTATE;
 		}
 		
 	},
+	
+	SECOND_ASTERISC_COMMENT_DELIMITER_STATE {
+		@Override
+		public State next(char character) {
+			if(character == '*') {
+				return SECOND_BAR_COMMENT_DELIMITER_STATE;
+			}
+				
+			return SECOND_ASTERISC_COMMENT_DELIMITER_STATE;
+		}
+		
+	},
+	
+	SECOND_BAR_COMMENT_DELIMITER_STATE {
+		@Override
+		public State next(char character) {
+			if(character == '/') {
+				return CORRECT_COMMENT_DELIMITER_STATE;
+			}
+				
+			return CommentDelimiterFinalStates.BADLYFORMEDCOMMENT_DELIMITER_FINALSTATE;
+		}
+		
+	},
+	
 	CORRECT_COMMENT_DELIMITER_STATE {
 
 		@Override
